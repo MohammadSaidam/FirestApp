df969ae9c896d4e1e958020cff0e3e384a271295
Mohmmed Saidam <msaidam58@gmail.com>
2023-02-25 10:26:19 +0200

    saidam

--- app/src/main/java/com/example/firestapp/MainActivity.kt
+++ app/src/main/java/com/example/firestapp/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.firestapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.firestapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        var binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.name.text = "Mohammed Saidam"
+
+
     }
 }
